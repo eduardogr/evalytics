50c962e8d2c996ec4028cba0999038eec9453f1d
Eduardo García <garciaruiz.edu@gmail.com>
2020-03-27 17:44:19 +0100

    Fix code, it couldn't be executed due to some failures

--- evalytics/server/di.py
+++ evalytics/server/di.py
@@ -1,11 +1,11 @@
 
 from dependency_injector import providers, containers
-from .adapters import FakeAdapter, GoogleSheetsAdapter
+from .repositories import FakeRepository, GoogleSheetsRepository
 
 
 class Reader(containers.DeclarativeContainer):
-    fake_reader = providers.Singleton(FakeAdapter)
-    google_sheets_reader = providers.Singleton(GoogleSheetsAdapter)
+    fake_reader = providers.Singleton(FakeRepository)
+    google_sheets_reader = providers.Singleton(GoogleSheetsRepository)
 
 
 class Module:
