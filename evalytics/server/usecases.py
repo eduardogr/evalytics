cd49d5c64ba6e94f87b98202aa64ed7439bf88e4
Eduardo García <garciaruiz.edu@gmail.com>
2020-04-02 18:26:06 +0200

    adding usecases tests

--- evalytics/server/usecases.py
+++ evalytics/server/usecases.py
@@ -1,26 +1,22 @@
 from .adapters import EmployeeAdapter
 from .core import DataRepository, CommunicationsProvider
 
-class SetupUseCase:
+class SetupUseCase(DataRepository):
 
     def execute(self):
-        repository = DataRepository()
-        setup = repository.setup()
+        setup = super().setup_storage()
         return setup
 
-class StartUseCase:
+class StartUseCase(DataRepository, CommunicationsProvider, EmployeeAdapter):
 
     def execute(self):
-        repository = DataRepository()
-        comms_provider = CommunicationsProvider()
-
-        employees = repository.get_employees()
+        employees = super().get_employees()
 
         reviews = []
         for employee in employees:
-            comms_provider.send_communication(
+            super().send_communication(
                 employee=employee,
-                data=EmployeeAdapter.build_eval_message(employee))
+                data=super().build_eval_message(employee))
             reviews.append(employee)
 
         return reviews
