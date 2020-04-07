540c6aa470c51bd4ac019c08f7dc26a558edfade
Eduardo García <garciaruiz.edu@gmail.com>
2020-04-07 11:58:47 +0200

    Fixing docker and adding simple error handling

--- evalytics/server/handlers.py
+++ evalytics/server/handlers.py
@@ -1,4 +1,6 @@
 import json
+import sys
+
 import tornado.web
 
 from .usecases import SetupUseCase, GetReviewersUseCase, SendMailUseCase
@@ -13,46 +15,78 @@ class SetupHandler(tornado.web.RequestHandler):
             setup_usecase = SetupUseCase()
             setup = setup_usecase.execute()
             self.finish({
+                'success': True,
                 'setup': setup.to_json()
             })
         except:
+            error = sys.exc_info()[0]
             self.finish({
                 'success': False,
-                'message': 'Something went wrong setting up evalytics',
+                'error': { 
+                    'message': error,
+                }
             })
 
 class ReviewersHandler(tornado.web.RequestHandler):
     path = r"/reviewers"
 
     async def get(self):
-        get_reviewers = GetReviewersUseCase()
-        reviewers = get_reviewers.execute()
+        try:
+            get_reviewers = GetReviewersUseCase()
+            reviewers = get_reviewers.execute()
 
-        self.finish({
-            'success': True,
-            'reviewers': [r.to_json() for uid, r in reviewers.items()]
-        })
+            self.finish({
+                'success': True,
+                'reviewers': [r.to_json() for uid, r in reviewers.items()]
+            })
+        except:
+            error = sys.exc_info()[0]
+            self.finish({
+                'success': False,
+                'error': { 
+                    'message': error,
+                }
+            })
 
 class SendMailHandler(tornado.web.RequestHandler, SendMailUseCase, Mapper):
     path = r"/sendmail"
 
     async def post(self):
-        reviewer_arg = self.get_argument('reviewers', "[]", strip=False)
-        reviewers = super().json_to_reviewer(json.loads(reviewer_arg))
+        try:
+            reviewer_arg = self.get_argument('reviewers', "[]", strip=False)
+            reviewers = super().json_to_reviewer(json.loads(reviewer_arg))
 
-        evals_sent, evals_not_sent = super().send_mail(reviewers)
-        self.finish({
-            'evals_sent': evals_sent,
-            'evals_not_sent': evals_not_sent
-        })
+            evals_sent, evals_not_sent = super().send_mail(reviewers)
+            self.finish({
+                'sucess': True,
+                'evals_sent': evals_sent,
+                'evals_not_sent': evals_not_sent
+            })
+        except:
+            error = sys.exc_info()[0]
+            self.finish({
+                'success': False,
+                'error': { 
+                    'message': error,
+                }
+            })
 
 class EvalsHandler(tornado.web.RequestHandler):
     path = r"/evals"
 
     async def get(self):
-        id = str(self.get_argument('id', None, True))
-
-        self.finish({
-            'id': id,
-            'eval': {},
-        })
+        try:
+            id = str(self.get_argument('id', None, True))
+            self.finish({
+                'sucess': True,
+                'id': id,
+                'eval': {},
+            })
+        except:
+            error = sys.exc_info()[0]
+            self.finish({
+                'success': False,
+                'error': { 
+                    'message': error,
+                }
+            })
