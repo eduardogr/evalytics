428e5ebd4f127a70de3e95af267a8a6b1188855b
Eduardo García <garciaruiz.edu@gmail.com>
2020-04-20 16:21:35 +0200

    Adding check for credentials.json file in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 VIRTUAL_ENV='env'
+CREDENTIALS='credentials.json'
 
 if [[ -d "$VIRTUAL_ENV" ]]
 then
-    echo "Python virtual environment $VIRTUAL_ENV exists on your repo path."
+    echo "[INFO] Python virtual environment $VIRTUAL_ENV exists on your repo path."
 else
     python3 -m venv env
+fi
+
+if [[ ! -f "$CREDENTIALS" ]]
+then
+    echo "[IMPORTANT] You need a $CREDENTIALS file"
 fi
 
 source env/bin/activate
